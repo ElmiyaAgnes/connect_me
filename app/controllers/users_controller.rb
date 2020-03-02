8fb42c8b73f20888b4358b30c272573a6dbc5e7c
Elmiya Karuthedath <ekaruthedath@adica.com.au>
2020-03-02 16:50:06 +1100

    Update user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,20 @@ class UsersController < ApplicationController
   end
  
   def update
+    if @user.update(permitted_params)
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
  
   private
+
   def set_user
     @user = User.find(params[:id])
   end
 
+  def permitted_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :phone, :bio)
+  end
 end
